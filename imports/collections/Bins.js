b88032e4164ebf1ed526c903075170b451baecaf
Adithya Kamath <adithyakamath96@gmail.com>
2020-09-21 14:30:48 +0530

    Added removal of bins, routing-v1

--- imports/collections/Bins.js
+++ imports/collections/Bins.js
@@ -1,15 +1,17 @@
-import {Mongo} from 'meteor/mongo';
+import { Mongo } from "meteor/mongo";
 
 Meteor.methods({
-    'bins.insert': function(){
-        return Bins.insert({
-            createdAt: new Date(),
-            content: '',
-            sharedWith: [],
-            ownerId: this.userId //getting this from Blaze and Meteor 
-        });
-    }
-})
+  "bins.insert": function () {
+    return Bins.insert({
+      createdAt: new Date(),
+      content: "",
+      sharedWith: [],
+      ownerId: this.userId, //getting this from Blaze and Meteor
+    });
+  },
+  "bins.remove": function (bin) {
+    return Bins.remove(bin);
+  },
+});
 
-export const Bins = new Mongo.Collection('bins');
-
+export const Bins = new Mongo.Collection("bins");
